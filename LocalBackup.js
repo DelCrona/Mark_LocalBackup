5ffbab63212fd8184bf61dd433604c7d617b8e2e
DelCrona <dmc12138@foxmail.com>
2024-04-11 00:47:52 +0800

    1.0.2：颜色标记相关

--- LocalBackup.js
+++ LocalBackup.js
@@ -176,7 +176,7 @@
                                 if (matchObj !== -1){
                                     objB.marks.forEach(function(markB){
                                     var existMark = mergedJSON[matchObj].marks.find(function(markA){
-                                        return markA.mark === markB.mark;
+                                        return (markA.mark === markB.mark && markA.bg_color === markB.bg_color);
                                     });
                                     if (!existMark){
                                         mergedJSON[matchObj].marks.push(markB);
